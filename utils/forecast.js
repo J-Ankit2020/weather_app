54a050a030c2482c1774653ed003c8363a0aeb5f
Ankit Jha <jhaa11701@gmail.com>
2022-01-18 19:28:52 +0530

    modified forecast data

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,15 +1,16 @@
-const request = require("postman-request");
+const request = require('postman-request');
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=10ce9d60323b59c1594ae99c32cadfa9&query=${latitude},${longitude}`;
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to weather services", undefined);
+      callback('Unable to connect to weather services', undefined);
     } else if (body.error) {
-      callback("Unable to find location", undefined);
+      callback('Unable to find location', undefined);
     } else {
       const temp = body.current.temperature,
-        feels = body.current.feelslike;
-      const str = `${body.current.weather_descriptions[0]}. It is currently ${temp} degrees out. It feels like ${feels} degrees out`;
+        feels = body.current.feelslike,
+        humidity = body.current.humidity;
+      const str = `${body.current.weather_descriptions[0]}. It is currently ${temp} degrees out. It feels like ${feels} degrees out. The humidity is ${humidity}% period`;
       callback(undefined, str);
     }
   });
